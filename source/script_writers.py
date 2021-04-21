8cd3998be6411a0062caae6e875d7dbbda42d5c1
M D <md@ms-macbook-pro.home>
2021-04-21 16:52:56 +0100

    scripts for glue

--- source/script_writers.py
+++ source/script_writers.py
@@ -1,4 +1,4 @@
-from source.utils import create_path
+from utils import create_path
 
 class DictToClass:
     def __init__(self, d):
@@ -13,16 +13,18 @@ def create_run_glue_script(p):
        --do_eval \
        --do_predict \
        --max_seq_length {p.max_len} \
-       --num_train_samples {p.tr_ep} \
-       --output_dir {p.output_dir} \
+       --num_train_epochs {p.tr_ep} \
        --train_file {p.file}_train.csv \
        --validation_file {p.file}_val.csv \
-       --test_file {p.file}_test.csv"
+       --test_file {p.file_test}_test.csv"
     
     if p.test_mode:
-        c = c + f" --max_train_samples {p.nr_test}\
+        c = c + f" --output_dir {p.output_dir}_test \
+        --max_train_samples {p.nr_test}\
         --max_val_samples {p.nr_test}\
         --max_test_samples {p.nr_test}"
+    else:
+        c = c + f" --output_dir {p.output_dir}"
     print(c)
 
 if __name__ == '__main__':
@@ -33,10 +35,27 @@ if __name__ == '__main__':
     p = dict(
         model_path = f'./models/{model_name}/',
         max_len = 128*2,
-        tr_ep = 15,
-        output_dir = f'./trained_models/{model_name}_{exp}/',
+        tr_ep = 1,
+        output_dir = f'./trained_models/{model_name}_{exp}',
         file = './data/train_samples/few_rel_train_bracket',
+        file_test = './data/train_samples/few_rel_val_bracket',
         test_mode = True,
         nr_test = 10
     )
+    print('='*60+' TEST '+'='*60)
+    print()
+    create_run_glue_script(p)
+    print()
+    p = dict(
+        model_path = f'./models/{model_name}/',
+        max_len = 128*2,
+        tr_ep = 15,
+        output_dir = f'./trained_models/{model_name}_{exp}/',
+        file = './data/train_samples/few_rel_train_bracket',
+        file_test = './data/train_samples/few_rel_val_bracket',
+        test_mode = False,
+        nr_test = 10
+    )
+    print('='*60+' REAL '+'='*60)
+    print()
     create_run_glue_script(p)
